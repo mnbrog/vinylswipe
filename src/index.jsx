3c48dea305d604d24b69e74c88903e9d016dbfda
mchael brogan <jaypreydoor@gmail.com>
2025-06-04 00:07:35 -0500

    jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
